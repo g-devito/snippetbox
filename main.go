9d3581ba51051e7a1fd0b875230d73ccf4c54bb0
Giovanni Chacon De Vito <giovanni.devito@tutamail.com>
2025-12-20 20:42:45 +0100

    add basic web server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	hello := "Hello World!"
+	_, err := w.Write([]byte(hello))
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func userAgent(w http.ResponseWriter, r *http.Request) {
+	userAgent := r.Header.Get("user-Agent")
+	_, err := w.Write([]byte(userAgent))
+	if err != nil {
+		log.Print(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello Wordl!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/agent", userAgent)
+
+	log.Print("running on port 4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
